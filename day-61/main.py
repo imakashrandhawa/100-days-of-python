be5b6cd9e8616fecbdea2a78baf10682e4700fd8
imakashrandhawa <98721872+imakashrandhawa@users.noreply.github.com>
2025-07-20 13:23:21 -0230

    login page made as required using WTForms documentation

--- day-61/main.py
+++ day-61/main.py
@@ -1,4 +1,11 @@
+import os
+
+from lib2to3.fixer_util import String
+
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms.fields.simple import StringField
+from wtforms.validators import DataRequired
 
 '''
 Red underlines? Install the required packages first: 
@@ -15,12 +22,21 @@ This will install the packages from requirements.txt for this project.
 
 
 app = Flask(__name__)
+app.secret_key=os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
+@app.route("/login")
+def login():
+    form=MyForm()
+    return render_template("login.html",form=form)
+
+class MyForm(FlaskForm):
+    name=StringField('Name',validators=[DataRequired()])
+    password=StringField('Password',validators=[DataRequired()])
 
 if __name__ == '__main__':
     app.run(debug=True)
