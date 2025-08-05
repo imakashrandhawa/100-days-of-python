5b12131f72deeda84db40c40144fecff35c8df45
imakashrandhawa <98721872+imakashrandhawa@users.noreply.github.com>
2025-08-05 19:18:35 -0230

    Updated following things in the files: 1) Add template to the defined routes 2) Used WTForms instead of regular label in add.html file and main.py 3) Received the data from add.html file and saved it in dictionary 4) Print the name of dictionary on the home page 5) Added p element to show Library is empty if the dictionary is empty using if statement

--- Day_63/main.py
+++ Day_63/main.py
@@ -1,4 +1,9 @@
+from symtable import Class
+
 from flask import Flask, render_template, request, redirect, url_for
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms.fields.simple import StringField
 
 '''
 Red underlines? Install the required packages first: 
@@ -14,18 +19,36 @@ This will install the packages from requirements.txt for this project.
 '''
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+bootstrap=Bootstrap5(app)
 
 all_books = []
+
+class Form(FlaskForm):
+    name=StringField("Book name: ")
+    author=StringField("Author: ")
+    rating=StringField("Rating: ")
+
 
 
 @app.route('/')
 def home():
-    pass
+    return render_template("index.html", all_books=all_books)
 
 
-@app.route("/add")
+@app.route("/add",methods=['POST','GET'])
 def add():
-    pass
+    form=Form()
+    if form.validate_on_submit():
+        all_books.append([
+            {
+                "title":form.name.data,
+                "author":form.author.data,
+                "rating":form.rating.data,
+            }
+        ])
+    print(all_books)
+    return render_template("add.html",form=form)
 
 
 if __name__ == "__main__":
